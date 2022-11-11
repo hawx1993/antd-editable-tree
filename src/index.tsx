510c47fea472ec49db4179074b8f7fa95b8b337c
hawx1993 <345823102@qq.com>
2022-11-11 15:46:11 +0800

    feat: 初始化工程配置文件

---
+++ src/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Button } from 'antd';
+import 'antd/dist/antd.css';
+
+const App = () => {
+  return (
+    <div>
+      123
+      <Button type="primary">click</Button>
+    </div>
+  );
+};
+export default App;
---
+++ vite.config.ts
@@ -0,0 +1,28 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+import path from 'path';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  css: {
+    preprocessorOptions: {
+      less: {
+        javascriptEnabled: true,
+      },
+    },
+  },
+  resolve: {
+    alias: [
+      {
+        find: /^~/,
+        replacement: '',
+      },
+      {
+        find: '@',
+        replacement: path.resolve(__dirname, './src'),
+      },
+    ],
+  },
+});
